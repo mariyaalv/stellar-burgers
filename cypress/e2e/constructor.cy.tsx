8418c6f5a80000873b3080d5cecb94e94625d4e4
mariyaalv <pavlovskayama.maria@yandex.ru>
2024-08-09 20:47:52 +0300

    added cypress comand for lines-selectors

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,38 +1,55 @@
+/// <reference path="../support/index.d.ts" />
+
 describe('add ingredients to constructor', function () {
+  Cypress.Commands.add('getElement', (value) => {
+    cy.get(`[data-cy=${value}]`);
+  });
+
+  //create value
+  const bun = 'bun';
+  const main = 'main';
+  const sauce = 'sauce';
+
+  const topOfBurger = 'bun-top';
+  const centerOfBurger = 'ingredients';
+  const bottonOfBurger = 'bun-botton';
+
+  const modal = 'modal';
+
   beforeEach(() => {
     // перехватили запрос и подставили в ответ свои данные
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
     cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
     cy.viewport(1300, 800);
-    cy.visit('http://localhost:4000');
+    cy.visit('/');
   });
 
   describe('should add elements to constructor', () => {
     it('should add bun to constructor', () => {
-      cy.get('[data-cy="bun"]').contains('Добавить').click();
+      cy.getElement(bun).contains('Добавить').click();
       //проверка на существование
-      cy.get('[data-cy=bun-top]').contains('Ингредиент 1').should('exist');
-      cy.get('[data-cy=bun-botton]').contains('Ингредиент 1').should('exist');
+      cy.getElement(topOfBurger).contains('Ингредиент 1').should('exist');
+      cy.getElement(bottonOfBurger).contains('Ингредиент 1').should('exist');
     });
 
     it('should add ingredient to constructor', () => {
-      cy.get('[data-cy="main"]').contains('Добавить').click();
-      cy.get('[data-cy="sauce"]').contains('Добавить').click();
+      cy.getElement(main).contains('Добавить').click();
+      cy.getElement(sauce).contains('Добавить').click();
 
-      cy.get('[data-cy=ingredients]').contains('Ингредиент 2').should('exist');
-      cy.get('[data-cy=ingredients]').contains('Ингредиент 4').should('exist');
+      cy.getElement(centerOfBurger).contains('Ингредиент 2').should('exist');
+      cy.getElement(centerOfBurger).contains('Ингредиент 4').should('exist');
     });
   });
 
   describe('should work correctly modals', () => {
     it('should open modal of ingredient', () => {
-      cy.get('[data-cy="bun"]').contains('Ингредиент 1').click();
-      cy.get('[data-cy=modal]').should('be.visible');
+      cy.getElement(bun).contains('Ингредиент 1').click();
+      cy.getElement(modal).should('be.visible');
     });
 
     it('should close modal of ingredient with click to icon', () => {
-      cy.get('[data-cy="bun"]').contains('Ингредиент 1').click();
-      cy.get('[data-cy=modal]')
+      cy.getElement(bun).contains('Ингредиент 1').click();
+      cy.getElement(modal)
         .should('contain', 'Ингредиент 1')
         .find('button')
         .click()
@@ -40,8 +57,8 @@ describe('add ingredients to constructor', function () {
     });
 
     it('should close modal of ingredient with click to overlay', () => {
-      cy.get('[data-cy="bun"]').contains('Ингредиент 1').click();
-      cy.get('[data-cy=modal]')
+      cy.getElement(bun).contains('Ингредиент 1').click();
+      cy.getElement(modal)
         .get('[data-cy=overlay]')
         .click('top', { force: true })
         .should('not.exist');
@@ -68,16 +85,16 @@ describe('add ingredients to constructor', function () {
 
     it('should create order', () => {
       // Создание заказа:
-      cy.get('[data-cy="bun"]').contains('Добавить').click();
-      cy.get('[data-cy="main"]').contains('Добавить').click();
-      cy.get('[data-cy="sauce"]').contains('Добавить').click();
+      cy.getElement(bun).contains('Добавить').click();
+      cy.getElement(main).contains('Добавить').click();
+      cy.getElement(sauce).contains('Добавить').click();
       cy.contains('Оформить заказ').click();
 
       // Проверяется, что модальное окно открылось и номер заказа верный.
-      cy.get('[data-cy=modal]').should('exist').and('contain', '123456');
+      cy.getElement(modal).should('exist').and('contain', '123456');
 
       // Закрывается модальное окно и проверяется успешность закрытия.
-      cy.get('[data-cy=modal]').find('button').click().should('not.exist');
+      cy.getElement(modal).find('button').click().should('not.exist');
 
       // Проверяется, что конструктор пуст
       cy.contains('Выберите булки');
